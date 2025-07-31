ff6cb41b1fcfa434e60a09acf89491f21cba9c73
SRamCoding <20rsebastian.trabajo@gmail.com>
2025-07-30 21:25:03 -0500

    commit

--- Querys.sql
+++ Querys.sql
@@ -223,37 +223,306 @@ ON t1.player_id = t2.player_id
 AND t1.event_date = t2.first_loggin;
 
 -- Q57:
+/*Write an SQL query to find the 
+customer_number for the customer 
+who has placed the largest
+number of orders.
+The test cases are generated so 
+that exactly one customer will have 
+placed more orders than any
+other customer.*/
+select T.customer_number
+from
+(select distinct customer_number, count(*) as t
+from Q57_Orders
+group by customer_number
+having t = (select count(*) as t
+from Q57_Orders
+group by customer_number
+order by t desc
+limit 1)) as T;
+
+create table if not exists Q57_Orders
+(
+	order_number int primary key,
+    customer_number int
+);
+
+insert into Q57_Orders (order_number, customer_number)
+values
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 3);
+
+-- Q58:
+/*Write an SQL query to report 
+all the consecutive available 
+seats in the cinema.
+Return the result table ordered 
+by seat_id in ascending order.
+The test cases are generated so 
+that more than two seats are 
+consecutively available.*/
+SELECT T.seat_id
+FROM
+(select seat_id, free,
+lag(free) over (order by seat_id) as anterior,
+lead(free) over (order by seat_id) as siguiente
+from Q58_Cinema) T
+WHERE T.free = 1 AND (T.anterior = 1 OR T.siguiente = 1);
+
+create table if not exists Q58_Cinema
+(
+	seat_id int primary key,
+    free bool
+);
+
+insert into Q58_Cinema (seat_id, free)
+values
+(1, 1),
+(2, 0),
+(3, 1),
+(4, 1),
+(5, 1);
+
+-- Q59:
 /*Write an SQL query to report the 
-device that is first logged in for 
-each player.
+names of all the salespersons who 
+did not have any orders related to
+the company with the name "RED".
 Return the result table in any order.*/
+SELECT DISTINCT name
+FROM Q59_SalesPerson 
+WHERE 
+sales_id NOT IN (select distinct t1.sales_id
+from Q59_Orders t1
+join Q59_Company t2
+on t1.com_id = t2.com_id
+where t2.name = 'RED');
 
+create table if not exists Q59_SalesPerson
+(
+	sales_id int primary key,
+    name varchar(10),
+    salary int,
+    commission_rate int,
+    hire_date date
+);
 
+create table if not exists Q59_Company
+(
+	com_id int primary key,
+    name varchar(10),
+    city varchar(10)
+);
 
+create table if not exists Q59_Orders
+(
+	order_id int primary key,
+    order_date date,
+    com_id int,
+    sales_id int,
+    amount int,
+    foreign key(com_id) references Q59_Company(com_id),
+    foreign key(sales_id) references Q59_SalesPerson(sales_id)
+);
 
+insert into Q59_SalesPerson (sales_id, name, salary, commission_rate, hire_date)
+values
+(1, 'John', 100000, 6, '2006-04-01'),
+(2, 'Amy', 12000, 5, '2010-05-01'),
+(3, 'Mark', 65000, 12, '2008-12-25'),
+(4, 'Pam', 25000, 25, '2005-01-01'),
+(5, 'Alex', 5000, 10, '2007-02-03');
 
+insert into Q59_Company (com_id, name, city)
+values
+(1, 'RED', 'Boston'),
+(2, 'ORANGE', 'New York'),
+(3, 'YELLOW', 'Boston'),
+(4, 'GREEN', 'Austin');
 
+insert into Q59_Orders (order_id, order_date, com_id, sales_id, amount)
+values
+(1, '2014-01-01', 3, 4, 10000),
+(2, '2014-02-01', 4, 5, 5000),
+(3, '2014-03-01', 1, 1, 50000),
+(4, '2014-04-01', 1, 4, 25000);
 
+-- Q60:
+/*Write an SQL query to report for 
+every three line segments whether 
+they can form a triangle.
+Return the result table in any order.*/
+select *, (case when (x + y > z and x + z > y and y + z > x) then 'Si' else 'No' end) as triangle 
+from Q60_Triangle;
 
+create table if not exists Q60_Triangle
+(
+	x int,
+    y int,
+    z int,
+    primary key (x, y, z)
+);
 
+insert into Q60_Triangle (x, y, z)
+values
+(13, 15, 30),
+(10, 20, 15);
 
+-- Q61:
+/*Write an SQL query to report the 
+shortest distance between any two 
+points from the Point table.*/
+select abs(t1.x - t2.x) as shortest
+from Q61_Point t1
+cross join Q61_Point t2
+where t1.x <> t2.x
+order by shortest asc
+limit 1;
 
+with OrderedPoints as 
+(select x,
+lead(x) over (order by x) as next_x
+from Q61_Point)
+select min(abs(x-next_x)) as shortest
+from OrderedPoints
+where next_x is not null; 
 
+/*O(n²) → O(n)*/
 
+create table if not exists Q61_Point
+(
+	x int primary key
+);
 
+insert into Q61_Point (x)
+values
+(-1),
+(0),
+(2);
 
+-- Q62:
+/*Write a SQL query for a report 
+that provides the pairs 
+(actor_id, director_id) where the 
+actor has cooperated with the 
+director at least three times.
+Return the result table in any order.*/
+select T.actor_id, T.director_id
+from
+(select actor_id, director_id, count(*) as t
+from Q62_ActorDirector
+group by actor_id, director_id
+having t >= 3) T;
 
+create table if not exists Q62_ActorDirector
+(
+	actor_id int,
+    director_id int,
+    timestamp int primary key
+);
 
+insert into Q62_ActorDirector (actor_id, director_id, timestamp) 
+values
+(1, 1, 0),
+(1, 1, 1),
+(1, 1, 2),
+(1, 2, 3),
+(1, 2, 4),
+(2, 1, 5),
+(2, 1, 6);
 
+-- Q63:
+/*Write an SQL query that reports 
+the product_name, year, and price 
+for each sale_id in the Sales table.
+Return the resulting table in any order.*/
+select t2.product_name, t1.year, t1.price
+from Q63_Sales t1
+join Q63_Product t2
+on t1.product_id = t2.product_id
+group by t2.product_name, t1.year, t1.price;
 
+create table if not exists Q63_Product
+(
+	product_id int primary key,
+    product_name varchar(15)
+);
 
+create table if not exists Q63_Sales
+(
+	sale_id int,
+    product_id int,
+    year int,
+    quantity int,
+    price int,
+    primary key(sale_id, year),
+	foreign key(product_id) references Q63_Product(product_id)
+);
 
+insert into Q63_Product (product_id, product_name)
+values 
+(100, 'Nokia'),
+(200, 'Apple'),
+(300, 'Samsung');
 
+insert into Q63_Sales (sale_id, product_id, year, quantity, price)
+values
+(1, 100, 2008, 10, 5000),
+(2, 100, 2009, 12, 5000),
+(7, 200, 2011, 15, 9000);
 
+-- Q64:
+/*Write an SQL query that reports 
+the average experience years of 
+all the employees for each project,
+rounded to 2 digits.
+Return the result table in any order..*/
+select t1.project_id, round(avg(t2.experience_years), 2) as average_years
+from Q64_Project t1
+join Q64_Employee t2
+on t1.employee_id = t2.employee_id
+group by t1.project_id;
 
+create table if not exists Q64_Employee
+(
+	employee_id int primary key,
+    name varchar(10),
+    experience_years int
+);
 
+create table if not exists Q64_Project
+(
+	project_id int,
+    employee_id int,
+    primary key(project_id, employee_id),
+    foreign key(employee_id) references Q64_Employee(employee_id)
+);
 
+insert into Q64_Employee (employee_id, name, experience_years)
+values
+(1, 'Khaled', 3),
+(2, 'Ali', 2),
+(3, 'John', 1),
+(4, 'Doe', 2);
 
+insert into Q64_Project (project_id, employee_id)
+values
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 1),
+(2, 4);
+
+-- Q65:
+/*Write an SQL query that reports the 
+best seller by total sales price, If 
+there is a tie, report them all.
+Return the result table in any order.*/
+select * from Q17_Product;
+select * from Q17_Sales;s
 
 
 
